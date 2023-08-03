c864a882fca54c8aef679068c2417394aedbf0da
Mohit Singh <101884246+Mohit2001Singh@users.noreply.github.com>
2023-08-03 12:08:55 +0530

    Added solution - LeetHub

--- Topological sort - GFG/topological-sort.cpp
+++ Topological sort - GFG/topological-sort.cpp
@@ -6,38 +6,70 @@ using namespace std;
 class Solution
 {
 	public:
-	void dfs(int i, int V, vector<int>adj[], stack<int>& st, vector<bool>& vis){
-	    if(vis[i] == true){
-	        return;
-	    }
-	    vis[i] = true;
-	    for(auto p : adj[i]){
-	        dfs(p, V, adj, st, vis);
-	    }
-	    st.push(i);
+// 	void dfs(int i, int V, vector<int>adj[], stack<int>& st, vector<bool>& vis){
+// 	    if(vis[i] == true){
+// 	        return;
+// 	    }
+// 	    vis[i] = true;
+// 	    for(auto p : adj[i]){
+// 	        dfs(p, V, adj, st, vis);
+// 	    }
+// 	    st.push(i);
 
-	}
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    vector<int> ans;
-	    stack<int> st;
-	    vector<bool> vis(V, false);
+// 	}
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    vector<int> ans;
+// 	    stack<int> st;
+// 	    vector<bool> vis(V, false);
+// 	    for(int i = 0;i<V;i++){
+// 	        if(vis[i] == false){
+// 	            dfs(i, V, adj, st, vis);
+// 	        }   
+// 	    }
+// 	    while(!st.empty()){
+// 	        ans.push_back(st.top());
+// 	        st.pop();
+// 	    }
+// 	   // for(auto i: ans){
+// 	   //     cout<<i<<" "<<endl;
+// 	   // }
+// 	    return ans;
+// 	}
+
+	vector<int> topoSort(int V, vector<int> adj[]){
+	    vector<int> inD(V, 0);
 	    for(int i = 0;i<V;i++){
-	        if(vis[i] == false){
-	            dfs(i, V, adj, st, vis);
+	        for(auto j : adj[i]){
+	            inD[j]++;
+	        }
+	    }
+	    queue<int> q;
+	    for(int i =0;i<V;i++){
+	        if(inD[i] ==0 ){
+	            q.push(i);
 	        }   
 	    }
-	    while(!st.empty()){
-	        ans.push_back(st.top());
-	        st.pop();
+	    vector<int> topo;
+
+	    while(!q.empty()){
+	        int a = q.front();
+	        q.pop();
+	        topo.push_back(a);
+	        
+	        for(auto i: adj[a]){
+	            inD[i]--;
+	            if(inD[i] == 0){
+	                q.push(i);
+	            }
+	        }
 	    }
-	   // for(auto i: ans){
-	   //     cout<<i<<" "<<endl;
-	   // }
-	    return ans;
+	    return topo;
 	}
+
+    
 };
 
 //{ Driver Code Starts.
